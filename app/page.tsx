5ab044d6da8ba706e77c58e320b0216b4e8c3c50
adxxtya <andronaut9@gmail.com>
2023-09-02 16:00:20 +0530

    Updated Metadata and Favicon

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,6 @@ import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import Main from "@/components/Main";
 import { ToggleProvider } from "@/components/ToggleContext";
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="w-full h-auto">
